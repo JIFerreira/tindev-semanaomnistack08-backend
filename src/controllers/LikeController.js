ee235502695fa377fbffdfe5b4a4ac2e325c6e9b
Jeferson Inacio Ferreira <jeferson.inacio.2@gmail.com>
2019-08-12 14:17:24 -0300

    enviando atualizações da branch de develop

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,34 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { devId } = req.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) res.status(400).json({ error: "dev not exists" });
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        console.log("loggedSocket");
+        req.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        console.log("targetSocket");
+        req.io.to(targetSocket).emit("match", loggedDev);
+      }
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
